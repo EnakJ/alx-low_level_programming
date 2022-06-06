52bc40440ae00196b89c4666d89f6504757b33e1
EnakJ <mamadoukanejunior@gmail.com>
2022-06-06 04:15:25 -0700

    task-3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	int i;
+	for (i = 97; i < 123; i++)
+	{
+		putchar(i);
+	}
+	for (i = 65; i < 91; i++)
+	{
+		putchar(i);
+	}
+	printf("");
+	return (0);
+}
