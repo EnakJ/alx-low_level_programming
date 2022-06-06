77a1bc74d629c57a03dba0007fcb421289def83e
EnakJ <mamadoukanejunior@gmail.com>
2022-06-06 06:26:15 -0700

    task-4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print alphabet
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		if (i == 101 || i == 113)
+		{
+			i++;
+		} else
+		putchar(i);
+	}
+	return (0);
+}
