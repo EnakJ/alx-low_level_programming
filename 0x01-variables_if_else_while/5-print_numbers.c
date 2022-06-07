a87ce8fd9d97bf9293b6072feeaf842202cf7451
EnakJ <mamadoukanejunior@gmail.com>
2022-06-06 18:03:33 -0700

    task-5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * main - print numbers
+ *main - printall single digit numbers of base 10
  *
- * Return: 0
+ *Return: 0
  */
 int main(void)
 {
 	int i;
 
-	for (i = 48; i < 58; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		putchar(i);
+		printf("%d", i);
 	}
-	putchar("\n");
+	printf("\n");
 	return (0);
 }
