a5622d12597ace2d9a7204d683dd4d91d898b34d
EnakJ <mamadoukanejunior@gmail.com>
2022-06-06 17:08:32 -0700

    task-5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar("\n");
+}
