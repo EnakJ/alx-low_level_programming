47241b92658f95fade7226a6bfa4a6a93d29e9b3
EnakJ <mamadoukanejunior@gmail.com>
2022-06-08 12:45:57 -0700

    task-1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,4 @@
-#include <unistd.h>
+#include <main.h>
 
 /**
  * main - function print_alphabet
@@ -7,11 +7,11 @@
  */
 void print_alphabet(void)
 {
-	int i;
+	char i;
 
 	for (i = 97; i < 123; i++)
 	{
-		putchar(i);
+		_putchar(i);
 	}
-	putchar("\n");
+	_putchar("\n");
 }
