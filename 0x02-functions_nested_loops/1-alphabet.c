b818cf86674ec9fc640c0570955ec0c0abe2c2b3
EnakJ <mamadoukanejunior@gmail.com>
2022-06-08 05:59:19 -0700

    task-1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - function print_alphabet
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		putchar(i);
+	}
+	printf("\n");
+}
