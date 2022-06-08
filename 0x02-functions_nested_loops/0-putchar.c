83d1547598b3aeb1708d4624ebf605fe06144a16
EnakJ <mamadoukanejunior@gmail.com>
2022-06-08 04:43:10 -0700

    task-0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,16 +3,19 @@
 
 /**
  *main - print putchar
+ *
  *Return: 0
  */
 int main(void)
 {
 	char c[] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
 	int n = 0;
+
 	while (n <= 7)
 	{
 		putchar(c[n]);
 		n++;
 	}
+	putchar("\n");
 	return (0);
 }
