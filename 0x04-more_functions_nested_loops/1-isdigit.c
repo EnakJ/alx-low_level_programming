ac0b65b36997ea0a775e73fe1d93ec9d6968d23d
Enak Junior <mamadoukanejunior@gmail.com>
2022-06-10 20:53:28 +0000

    task-1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - check the upcase character
+ * @c: The digit to be checked
+ * Return: 1 if c is a digit, 0 else
+ */
+int _isupper(char c)
+{
+        if (c >= '0' && c <= '9')
+        {
+                return (1);
+        }
+        else
+        {
+                return (0);
+        }
+}
+
