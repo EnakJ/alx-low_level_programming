060d9a4c66e19f52ab64650977916f2bced29baf
Enak Junior <mamadoukanejunior@gmail.com>
2022-06-10 23:34:51 +0000

    task-5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,9 +14,9 @@ void print_more_numbers(void)
 		for (i = 0; i <= 14; i++)
 		{
 			if (i > 9)
-				putchar((i / 10) + '0');
-			putchar((i % 10) + '0');
+				_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
 		}
-		putchar('\n');
+		_putchar('\n');
 	}
 }
