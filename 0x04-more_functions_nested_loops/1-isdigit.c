47588716b2c44b56dee688cb3b06c0f78fc35e3d
Enak Junior <mamadoukanejunior@gmail.com>
2022-06-10 21:00:41 +0000

    task-1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -7,13 +7,12 @@
  */
 int _isdigit(char c)
 {
-        if (c >= '0' && c <= '9')
-        {
-                return (1);
-        }
-        else
-        {
-                return (0);
-        }
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
-
