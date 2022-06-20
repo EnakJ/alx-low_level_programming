fad17b02802b166290a63f7e3e2175bea0b33662
Enak Junior <mamadoukanejunior@gmail.com>
2022-06-20 17:45:20 +0000

    task-0

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,7 @@
-#ifdef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
+
+#include <stddef.h>
 
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
@@ -9,5 +11,7 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
+
 
 #endif MAIN_H
