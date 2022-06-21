c33f1bdb5a6da46609c3b86d00dca7e470a6fc47
Enak Junior <105060872+EnakJ@users.noreply.github.com>
2022-06-21 19:12:08 +0000

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,7 +3,7 @@
 /**
  * memset - fill memory with a constant byte.
  *
- * Description: Fills the first n bytes of the memory area 
+ * Description: Fills the first n bytes of the memory area
  * pointed to by @s with the constant byte @c.
  *
  * @s: A pointer to the memory area to be filled.
@@ -19,5 +19,6 @@ void *_memset(void *s, int c, size_t n)
 
 	for (index = 0; index < n; index++)
 		memory[index] = value;
+
 	return (memory);
 }
