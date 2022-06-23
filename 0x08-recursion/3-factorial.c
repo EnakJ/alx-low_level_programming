3ebb391f839d83a4f4ba6372d7f75a79fd530d0a
Enak Junior <mamadoukanejunior@gmail.com>
2022-06-23 09:39:34 +0000

    task-3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * factorial - returns the factorial of a given number
+ *@n: an integer
+ * Return: the factorial of @n
+ */
+int factorial(int n)
+{
+	int fact = 1;
+
+	if (n == 0)
+		fact = 1;
+	if (n == 1)
+		fact = 1;
+	if (n != 0 && n != 1)
+	{
+		fact = n * factorial(n - 1);
+	}
+	return (fact);
+}
--- 0x08-recursion/main.c
+++ 0x08-recursion/main.c
@@ -7,10 +7,16 @@
  * Return: Always 0.
  */
 int main(void)
-{
-	int n;
+{	
+	int r;
 
-	n = _strlen_recursion("Corbin Coleman");
-	printf("%d\n", n);
+	r = factorial(1);
+	printf("%d\n", r);
+	r = factorial(5);
+	printf("%d\n", r);
+	r = factorial(10);
+	printf("%d\n", r);
+	r = factorial(-1024);
+	printf("%d\n", r);
 	return (0);
 }
