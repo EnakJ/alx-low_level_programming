943fa5d8c98b9ab361f0382678b00fd14e58504b
Enak Junior <mamadoukanejunior@gmail.com>
2022-06-23 09:16:45 +0000

    task-1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - print a string in inverse
+ *
+ * @s: strint to be printed
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
+
